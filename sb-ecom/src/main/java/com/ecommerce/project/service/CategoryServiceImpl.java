3c852925171194dafe3b2778e5b8f2c930713e61
Mihir Singh <mihir304singh@gmail.com>
2026-08-03 04:37:10 +0530

    Return empty category lists successfully

--- sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -35,9 +35,6 @@ public class CategoryServiceImpl implements CategoryService{
         Page<Category> categoryPage = categoryRepository.findAll(pageDetails);
 
         List<Category> categories = categoryPage.getContent();
-        if (categories.isEmpty())
-            throw new APIException("No category created till now.");
-
         List<CategoryDTO> categoryDTOS = categories.stream()
                 .map(category -> modelMapper.map(category, CategoryDTO.class))
                 .toList();
