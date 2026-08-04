683c04ad7db3222ce86aa2d7189535077f4cff4f
Mihir Singh <mihir304singh@gmail.com>
2026-08-04 21:10:20 +0530

    revert: restore Product.java IDENTITY generation type and application.properties

--- sb-ecom/src/main/java/com/ecommerce/project/model/Product.java
+++ sb-ecom/src/main/java/com/ecommerce/project/model/Product.java
@@ -21,7 +21,7 @@ import java.util.List;
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long productId;
 
     @NotBlank
